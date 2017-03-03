72c59036a53041b3ec899ac3ebd18796ea336f8c
drishu <aszilagyi@live.com>
2017-03-03 10:43:21 +0100

    accordion fixes for NEMA-222

--- modules/features/nems_accordion/plugin/plugin.js
+++ modules/features/nems_accordion/plugin/plugin.js
@@ -6,70 +6,155 @@
 (function ($) {
   CKEDITOR.plugins.add('nems_accordion_plugin', {
     init: function (editor) {
-      editor.addCommand('add_accordion', {
-        exec : function (editor) {
-          // Create the wrapper div.
-          var accr = editor.document.createElement('div');
-          accr.setAttribute('class', 'accordion');
-
-          // Create an h3 element to the title.
-          var accr_title = editor.document.createElement('h3');
-          accr_title.setText('Accordion Title');
-
-          // Append the title into wrapper div.
-          accr.append(accr_title);
-
-          // Create a div element for the content of accordion.
-          var accr_content = editor.document.createElement('div');
-          accr_content.setAttribute('class', 'accordion-content');
-
-          // Create the first paragraph.
-          var accr_content_p1 = editor.document.createElement('p');
-          accr_content_p1.setText('Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.');
-
-          // Create the second paragraph.
-          var accr_content_p2 = editor.document.createElement('p');
-          accr_content_p2.setText('Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas sed diam eget risus varius blandit sit amet non magna. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla vitae elit libero, a pharetra augue. Nullam quis risus eget urna mollis ornare vel eu leo.');
-
-          // Append the paragraphs into the content div.
-          accr_content.append(accr_content_p1);
-          accr_content.append(accr_content_p2);
-
-          // Append the content div into accordion wrapper.
-          accr.append(accr_content);
-
-          // Create a hidden paragraph.
-          var accr_content_phidden = editor.document.createElement('p');
-          accr_content_phidden.setAttribute('class', 'hidden');
-
-          // Append the hidden paragraph into accordion wrapper.
-          accr.append(accr_content_phidden);
-
-          // Validate accordion.
-          var parents = editor.getSelection().getStartElement().getParents();
-          var parentIndex;
-          var isAccordion;
-
-          for (parentIndex = 0; parentIndex < parents.length; ++parentIndex) {
-            if (parents[parentIndex].hasClass('accordion')) {
-              parents[parentIndex].append(accr_title);
-              parents[parentIndex].append(accr_content);
-              isAccordion = true;
-            }
-          }
-
-          // Insert the accordion HTML to the editor if it's not an existing accordion.
-          if (isAccordion != true) {
-            editor.insertElement(accr);
-          }
-
+      // Utility functions.
+      function nemsWrapper(el) {
+        if (el.getAttribute) {
+          return el.getAttribute('class') == 'accordion';
         }
-      });
+      };
+      function nemsPane(el) {
+        if (el.getAttribute) {
+          return el.getAttribute('class') == 'accordion-content';
+        }
+      };
+
+      // Add single button.
       editor.ui.addButton('nems_accordion_plugin_button', {
         label: 'Insert an Accordion',
-        command: 'add_accordion',
+        command: 'nemsAddAccordion',
         icon: this.path + 'images/button.png'
       });
+
+      // Add CSS for edition state.
+      var cssPath = this.path + 'accordion.css';
+      editor.on('mode', function () {
+        if (editor.mode === 'wysiwyg') {
+          this.document.appendStyleSheet(cssPath);
+        }
+      });
+
+      // Prevent nesting by disabling button.
+      editor.on('selectionChange', function (evt) {
+        if (editor.readOnly) {
+          return;
+        }
+        var command = editor.getCommand('nemsAddAccordion');
+        var element = evt.data.path.lastElement && evt.data.path.lastElement.getAscendant(nemsWrapper, true);
+        if (element) {
+          command.setState(CKEDITOR.TRISTATE_DISABLED);
+        }
+        else {
+          command.setState(CKEDITOR.TRISTATE_OFF);
+        }
+      });
+
+
+      var allowedContent = 'div h3 p(!accordion)';
+
+      editor.addCommand('nemsAddAccordion', {
+        allowedContent: allowedContent,
+
+        exec : function (editor) {
+          // Create the wrapper div.
+          var wrapper = new CKEDITOR.dom.element.createFromHtml(
+            '<div class="accordion">' +
+            '<h3>Accordion title</h3>' +
+            '<div class="accordion-content"><p>New accordion content</p></div>' +
+            '</div>'
+          );
+
+          editor.insertElement(wrapper);
+        }
+      });
+      editor.addCommand('nemsAccordionTabBefore', {
+        allowedContent: allowedContent,
+
+        exec: function (editor) {
+          var element = editor.getSelection().getStartElement();
+          var newHeader = new CKEDITOR.dom.element.createFromHtml('<h3>New accordion title</h3>');
+          var newContent = new CKEDITOR.dom.element.createFromHtml('<div class="accordion-content"><p>New accordion content</p></div>');
+          if (element.getAscendant(nemsPane, true)) {
+            element = element.getAscendant(nemsPane, true).getPrevious();
+          }
+          else {
+            element = element.getAscendant('h3', true);
+          }
+          newHeader.insertBefore(element);
+          newContent.insertBefore(element);
+        }
+      });
+      editor.addCommand('nemsAccordionTabAfter', {
+        allowedContent: allowedContent,
+
+        exec: function (editor) {
+          var element = editor.getSelection().getStartElement();
+          var newHeader = new CKEDITOR.dom.element.createFromHtml('<h3>New Accordion title</h3>');
+          var newContent = new CKEDITOR.dom.element.createFromHtml('<div class="accordion-content"><p>New accordion content</p></div>');
+          if (element.getAscendant('h3', true)) {
+            element = element.getAscendant('h3', true).getNext();
+          }
+          else {
+            element = element.getAscendant(nemsPane, true);
+          }
+          newContent.insertAfter(element);
+          newHeader.insertAfter(element);
+        }
+      });
+      editor.addCommand('nemsRemoveAccordionTab', {
+        exec: function (editor) {
+          var element = editor.getSelection().getStartElement();
+          var a;
+          if (element.getAscendant('h3', true)) {
+            a = element.getAscendant('h3', true);
+            a.getNext().remove();
+            a.remove();
+          }
+          else {
+            a = element.getAscendant(nemsPane, true);
+            if (a) {
+              a.getPrevious().remove();
+              a.remove();
+            }
+            else {
+              element.remove();
+            }
+          }
+        }
+      });
+
+      // Context menu.
+      if (editor.contextMenu) {
+        editor.addMenuGroup('nemsAccordionGroup');
+        editor.addMenuItem('nemsAccordionTabBefore', {
+          label: Drupal.t('Add accordion tab before'),
+          icon: this.path + 'images/button.png',
+          command: 'nemsAccordionTabBefore',
+          group: 'nemsAccordionGroup'
+        });
+        editor.addMenuItem('nemsAccordionTabAfter', {
+          label: Drupal.t('Add accordion tab after'),
+          icon: this.path + 'images/button.png',
+          command: 'nemsAccordionTabAfter',
+          group: 'nemsAccordionGroup'
+        });
+        editor.addMenuItem('nemsRemoveAccordionTab', {
+          label: Drupal.t('Remove accordion tab'),
+          icon: this.path + 'images/button.png',
+          command: 'nemsRemoveAccordionTab',
+          group: 'nemsAccordionGroup'
+        });
+
+        editor.contextMenu.addListener(function (element) {
+          var parentEl = element.getAscendant(nemsWrapper, true);
+          if (parentEl) {
+            return {
+              nemsAccordionTabBefore: CKEDITOR.TRISTATE_OFF,
+              nemsAccordionTabAfter: CKEDITOR.TRISTATE_OFF,
+              nemsRemoveAccordionTab: CKEDITOR.TRISTATE_OFF
+            };
+          }
+        });
+      }
     }
   });
 })(jQuery);
